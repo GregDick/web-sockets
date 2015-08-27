66786514e908a434683b9531b999b0e79e72421e
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-27 15:53:40 -0500

    set up message with the socket. append messages to page

--- server/app.js
+++ server/app.js
@@ -15,5 +15,19 @@ var io = socketio(server);
 
 io.on('connection', function(socket){
   console.log('Client connected:', socket.id);
+
+  socket.on('disconnect', function(){
+    console.log('Client disconnected:', socket.id);
+  });
+
+  socket.on('chatMessage', function(msg){
+    console.log('Chat Message recieved', msg);
+    // to one socket
+    // socket.emit('chatMessage', {toOne : msg});
+    // to everyone except the sender
+    // socket.broadcast.emit('chatMessage', {toOthers : msg});
+    //to everyone
+    io.emit('chatMessage', msg);
+  });
 });
 
